a34a783ea300602db7dfeaeb545e1951748a5184
mrinnnmoy <mrinmoy202000@gmail.com>
2023-01-30 11:33:43 +0530

    create registration-api

--- app.js
+++ app.js
@@ -8,14 +8,14 @@ const cookieParser = require("cookie-parser");
 const app = express();
 
 // Configure ENV File & Require Connection File
-dotenv.config({path: './config.env'});
-require('./db/conn');
+dotenv.config({ path: "./config.env" });
+require("./db/conn");
+const port = process.env.PORT;
 
 // Require Model
-const Users = require('./models/userSchema');
+const Users = require("./models/userSchema");
 
-
-// These Method is used to Get Data and Cookies from FrontEnd
+// These Method is used to Get Data & Cookies from FrontEnd
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -24,7 +24,32 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+// Registration
+app.post("/register", async (req, res) => {
+  try {
+    // Get body or Data
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const createUser = new Users({
+      username: username,
+      email: email,
+      password: password,
+    });
+
+    // Save Method is used to create user or insert user
+    // But Before Saving or Inserting, Password will Hash
+    // Because of Hashing. After Hash, it will save to DB
+    const created = await createUser.save();
+    console.log(created);
+    res.status(200).send("Registered");
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
 // Run Server
-app.listen(3001, () => {
+app.listen(port, () => {
   console.log("Server is Listening");
 });
