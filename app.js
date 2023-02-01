9bcb7ac93c9cc6bd20be0b970648fa007d790a22
mrinnnmoy <mrinmoy202000@gmail.com>
2023-02-01 10:02:13 +0530

    build msgSchema&api

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const port = process.env.PORT;
 
 // Require Model
 const Users = require("./models/userSchema");
+const Message = require('./models/msgSchema');
 
 // These Method is Used to Get Data and Cookies from FrontEnd
 app.use(express.json());
@@ -76,6 +77,31 @@ app.post("/login", async (req, res) => {
     } else {
       res.status(400).send("Invalid Credentials");
     }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+});
+
+// Message
+app.post("/message", async (req, res) => {
+  try {
+    // Get body or Data
+    const name = req.body.name;
+    const email = req.body.email;
+    const message = req.body.message;
+
+    const sendMsg = new Message({
+      name: name,
+      email: email,
+      message: message,
+    });
+
+    // Save Method is Used to Create User or Insert User
+    // But Before Saving or Inserting, Password will Hash
+    // Because of Hashing. After Hash, It will save to DB
+    const created = await sendMsg.save();
+    console.log(created);
+    res.status(200).send("Sent");
   } catch (error) {
     res.status(400).send(error);
   }
